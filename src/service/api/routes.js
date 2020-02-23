17c313b3024addab9e6c7736e57c2c3d3968a20b
Gabriel-Monteiro7 <gabrielmont713@gmail.com>
2020-02-23 18:30:23 -0300

    Criaçao da api usada na aplicaçao

---
+++ src/service/api/routes.js
@@ -0,0 +1,64 @@
+import { Router } from "express";
+
+const axios = require("axios");
+let cheerio = require("cheerio");
+let data = [];
+let iconv = require("iconv-lite");
+const puppeteer = require("puppeteer");
+
+const routes = new Router();
+
+routes.get("/", (req, res) => {
+  axios.get("https://www.tricae.com.br/brinquedos/?page=1").then(response => {
+    let body = response.data;
+    let $ = cheerio.load(body, { decodeEntities: false });
+    let value = [];
+    $(".product-box").each((indice, element) => {
+      value.push({
+        img: $(element)
+          .find("img")
+          .attr("data-original"),
+        titulo: $(element)
+          .find(".product-box-title")
+          .text(),
+        labelMarca: $(element)
+          .find(".product-box-brand")
+          .text(),
+        preco: $(element)
+          .find(".product-box-price-from")
+          .text()
+      });
+    });
+    value.shift()
+    res.json(value)
+  });
+  // let scrape = async () => {
+  //   const browser = await puppeteer.launch();
+
+  //   const page = await browser.newPage();
+  //   page.setDefaultNavigationTimeout(10000000);
+  //   await page.goto("https://www.tricae.com.br/brinquedos");
+  //   const result = await page.evaluate(() => {
+  //     let produtos=[]
+
+  //     document
+  //       .querySelectorAll(".product-box")
+  //       .forEach(produto => {produtos.push({
+  //         labelMarca:(produto.getElementsByClassName('product-box-brand')[0]),
+  //         // preco:produto.getElementsByClassName('x-shelf__old-price')[0].innerText,
+  //         label:(produto.getElementsByClassName('product-box-title').innerText),
+  //         preco:(produto.getElementsByClassName('product-box-price-from').innerText),
+  //         img:produto.getElementsByTagName('img')[0].getAttribute('data-original')
+  //       });console.log(produto)}
+  //       );
+  //     return produtos;
+  //   });
+
+  //   browser.close();
+  //   return result;
+  // };
+  // scrape().then(value => {
+  //   res.json(value)
+  // });
+});
+export default routes;
