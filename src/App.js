fef7da8ab44d13ec36d0cc18714c59de1a69155b
Gabriel-Monteiro7 <gabrielmont713@gmail.com>
2020-02-29 13:27:20 -0300

    Configuraçao do context-api simples para a aplicaçao

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import React from "react";
 import Footer from "./components/default/footer/Footer";
 import NavBar from "./components/default/navbar/Navbar";
 import Routes from "./routes";
-import ButtonTop from './components/default/ButtonTop'
-import {BrowserRouter } from "react-router-dom";
+import ButtonTop from "./components/default/ButtonTop";
+import { BrowserRouter } from "react-router-dom";
 import { GlobalStyle } from "./styles/global";
+import ProdutosContextProvider from "./contexts/ProdutosContext";
 function App() {
   return (
     <BrowserRouter>
-      <NavBar />
-      <Routes />
-      <ButtonTop/>
-      <Footer />
-      <GlobalStyle/>
+      <ProdutosContextProvider>
+        <NavBar />
+        <Routes />
+        <ButtonTop />
+        <Footer />
+        <GlobalStyle />
+      </ProdutosContextProvider>
     </BrowserRouter>
   );
 }
